d59b47d9c92b0c72113f3ba92673d2e06de7be2a
“Corentin <Lej.corentin@gmail.com>
2015-10-24 16:54:43 +0200

    modification couleur

--- color.js
+++ color.js
@@ -1,4 +1,4 @@
-function PixelColor (x,y,size,color) {
+/*function PixelColor (x,y,size,color) {
 	console.log(x, y, size, color);
 	Pixel.call(this,x,y,size,color);
 }
@@ -22,4 +22,46 @@ PixelColor.prototype.set_color = function(color) {
 	this.$el.css({
 		backgroundColor : that.color,
 	})
+}
+*/
+
+function Color(width, height, Color) {
+ 	this.$els = $('<div></div>');
+
+ 	this.set_size(width, height);
+ 	this.init();
+}
+
+Color.prototype = {
+
+	set_size: function(width, height) {
+		this.width  = 50;
+		this.height = 50;
+		this.$els.css({
+		width : this.width + 'px',
+		height : this.height + 'px',
+	})
+},
+
+ init: function() {
+
+		this.Color = [
+		'#9365B8', '#475577', '#41A85F', '#00A885',
+		'#3D8EB9', '#2969B0', '#553982', '#28324E',
+
+		'#F7DA64', '#FBA026', '#EB6B56', '#E25041', 
+		'#A38F84', '#EFEFEF', '#FAC51C', '#F37934',
+		'#D14841', '#B8312F', '#75706B', '#D1D5D8',
+	 	];
+
+	   	for (var i = 0; i < this.Color.length - 1; i++ ) {
+	    	//console.log(i);
+		    this.$els.clone().appendTo(PixelArt.$toolsColor);
+		    this.$els.removeClass().addClass('list color' + i);
+		    this.$els.css('background-color', this.Color[i]);
+	   	};
+
+   		PixelArt.$toolsColor.find('div').eq(0).remove();
+
+  }
 }
--- main.js
+++ main.js
@@ -9,25 +9,101 @@ var pixelArt = {
 
 		var self = this;
 
-		this.init_tools();
+		this.init_color();
 
 		this.init_draw();
 
-		this.$draw.on('mousedown', function(e) {
+		var point 	= true;
+		var line 	= false;
+		var gomme	= false;
+	
+		$('#point').on('click', function() {
+			point 	= true;
+			line 	= false;
+			gomme	= false;
 
-			var offset = self.$draw.offset(),
-				dx = e.clientX - parseInt(offset.left),
-				dy = e.clientY - parseInt(offset.top);
+			console.log(point, line, gomme);
 
-			var x = Math.floor(dx/self.pixel_dimension)+1;
-			var y = Math.floor(dy/self.pixel_dimension)+1;
+		});
+
+		$('#ligne').on('click', function() {
+			point 	= false;
+			line 	= true;
+			gomme	= false;
+
+			console.log(point, line, gomme);
+
+		});
+
+		$('#gomme').on('click', function() {
+			point 	= false;
+			line 	= false;
+			gomme	= true;	
+
+			console.log(point, line, gomme);
+
+		});
 
 
-			var p =  new PixelColor(x,y,self.pixel_dimension, color);	
+		if (point === true && line === false && gomme === false) {
+			this.$draw.on('mousedown', function(e) {
 
-			self.pixels.push(p);
+				var offset = self.$draw.offset(),
+					dx = e.clientX - parseInt(offset.left),
+					dy = e.clientY - parseInt(offset.top);
 
-		})	
+				var x = Math.floor(dx/self.pixel_dimension)+1;
+				var y = Math.floor(dy/self.pixel_dimension)+1;
+
+
+				var p =  new Pixel(x,y,self.pixel_dimension, Color.color);
+				console.log(point, line, gomme, 'point');
+				
+
+				self.pixels.push(p);
+
+			})
+		}
+
+		else if (point === false && line === true && gomme === false) {
+			this.$draw.on('mousemove', function(e) {
+
+				var offset = self.$draw.offset(),
+					dx = e.clientX - parseInt(offset.left),
+					dy = e.clientY - parseInt(offset.top);
+
+				var x = Math.floor(dx/self.pixel_dimension)+1;
+				var y = Math.floor(dy/self.pixel_dimension)+1;
+
+
+				var p =  new Pixel(x,y,self.pixel_dimension, '#fff');
+				console.log(point, line, gomme, 'ligne');
+
+
+				self.pixels.push(p);
+
+			})
+		}
+
+		else if (point === false && line === false && gomme === true) {
+			this.$draw.on('mousedown', function(e) {
+
+				var offset = self.$draw.offset(),
+					dx = e.clientX - parseInt(offset.left),
+					dy = e.clientY - parseInt(offset.top);
+
+				var x = Math.floor(dx/self.pixel_dimension)+1;
+				var y = Math.floor(dy/self.pixel_dimension)+1;
+
+
+				var p =  new Pixel(x,y,self.pixel_dimension, '#646809');
+				console.log(point, line, gomme, 'gomme');
+
+
+				self.pixels.push(p);
+
+			})
+		}			
 	},	
 
 	init_draw: function() {
@@ -46,13 +122,13 @@ var pixelArt = {
 		})
 	},
 
-	init_tools: function() {
-		var t = new Tools(tools);
-	}, 
+	init_color: function() {
+		this.$toolsColor = $('#toolbar');
+	}
 }
 
 $(function(){
-	pixelArt.init_tools();	
 	pixelArt.init();
-	pixelArt.init_draw();	
+	pixelArt.init_draw();
+	pixelArt.init_color();
 })
